9a800232aba0216f09cdd4f40c0c0cbe5bf96080
Jaime_Liu <jaime.liu@proximitycr.com>
2020-01-22 08:23:20 -0600

    Fix error CSP Facebook connect

--- src/components/SocialMediaButtons/SocialMediaButtons.js
+++ src/components/SocialMediaButtons/SocialMediaButtons.js
@@ -5,61 +5,16 @@ import Swal from 'sweetalert2';
 import axios from 'axios';
 
 const SocialMediaButtons = props => {
-  const [socialMediaName, setSocialMediaName] = useState('');
-  const handleResponse = data => {
-    Swal.fire({
-      title: 'Success',
-      text: 'Please verify your email to get the verify code',
-      icon: 'success',
-    });
-    const newAccount = { Facebook: { username: data.name, picture: data.picture.data.url } };
-    props.setNewAccount(newAccount);
-    const verificationCode = Math.floor(
-      Math.pow(10, 6 - 1) + Math.random() * (Math.pow(10, 6) - Math.pow(10, 6 - 1) - 1)
-    );
-    props.setVerificationCode(verificationCode.toString());
-    console.log(verificationCode);
-    const mailData = {
-      destination: props.currentUser.attributes.email,
-      text: 'Your verification code is ' + verificationCode,
-    };
-    // const account={}
-    // axios
-    //   .post('/api/sendMail', mailData)
-    //   .then(r => {
-    //     console.log(r);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
-    props.setStateButtons(true);
-    console.log(data);
-  };
-  const handleOnclickFacebook = e => {
-    setSocialMediaName(e.target.textContent);
-  };
   return (
     <div className={css.linkAccountContainer}>
       <div className={css.colButtons}>
         <button>Instagram</button>
       </div>
       <div className={css.colButtons}>
-        <FacebookLogin
-          appId="2644611702444575"
-          autoLoad={false}
-          size="medium"
-          scope="user_friends"
-          fields="name,email,picture"
-          callback={handleResponse}
-          onClick={handleOnclickFacebook}
-          cssClass={css.colButtons}
-          textButton="Facebook"
-        >
-          Facebook
-        </FacebookLogin>
+        <button>Facebook</button>
       </div>
       <div className={css.colButtons}>
-        <button onClick={handleOnclickFacebook}>Vsco</button>
+        <button>Vsco</button>
       </div>
       <div className={css.colButtons}>
         <button>Pinterest</button>
