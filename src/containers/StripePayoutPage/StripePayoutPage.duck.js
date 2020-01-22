yamahiro-offto/ftw-daily

Update StripePayoutPage

src/containers/StripePayoutPage/StripePayoutPage.duck.js
@@ -1,11 +1,14 @@
+import pick from 'lodash/pick';
 import {
   createStripeAccount,
+  updateStripeAccount,
+  fetchStripeAccount,
 } from '../../ducks/stripeConnectAccount.duck';
 import { fetchCurrentUser } from '../../ducks/user.duck';
 
 // ================ Action types ================ //
 
-export const SET_INITIAL_STATE = 'app/StripePayoutPage/SET_INITIAL_STATE';
+export const SET_INITIAL_VALUES = 'app/StripePayoutPage/SET_INITIAL_VALUES';
 export const SAVE_PAYOUT_DETAILS_REQUEST = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_REQUEST';
 export const SAVE_PAYOUT_DETAILS_SUCCESS = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_SUCCESS';
 export const SAVE_PAYOUT_DETAILS_ERROR = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_ERROR';
@@ -15,13 +18,14 @@ export const SAVE_PAYOUT_DETAILS_ERROR = 'app/StripePayoutPage/SAVE_PAYOUT_DETAI
 const initialState = {
   payoutDetailsSaveInProgress: false,
   payoutDetailsSaved: false,
+  fromReturnURL: false,
 };
 
-export default function payoutPreferencesPageReducer(state = initialState, action = {}) {
-  const { type } = action;
+export default function reducer(state = initialState, action = {}) {
+  const { type, payload } = action;
   switch (type) {
-    case SET_INITIAL_STATE:
-      return initialState;
+    case SET_INITIAL_VALUES:
+      return { ...initialState, ...payload };
 
     case SAVE_PAYOUT_DETAILS_REQUEST:
       return { ...state, payoutDetailsSaveInProgress: true };
@@ -37,8 +41,9 @@ export default function payoutPreferencesPageReducer(state = initialState, actio
 
 // ================ Action creators ================ //
 
-export const setInitialState = () => ({
-  type: SET_INITIAL_STATE,
+export const setInitialValues = initialValues => ({
+  type: SET_INITIAL_VALUES,
+  payload: pick(initialValues, Object.keys(initialState)),
 });
 
 export const savePayoutDetailsRequest = () => ({
@@ -53,18 +58,32 @@ export const savePayoutDetailsSuccess = () => ({
 
 // ================ Thunks ================ //
 
-export const savePayoutDetails = values => (dispatch, getState, sdk) => {
+export const savePayoutDetails = (isUpdateCall, values) => (dispatch, getState, sdk) => {
+  const upsertThunk = isUpdateCall ? updateStripeAccount : createStripeAccount;
   dispatch(savePayoutDetailsRequest());
 
-  return dispatch(createStripeAccount(values))
-    .then(() => dispatch(savePayoutDetailsSuccess()))
+  return dispatch(upsertThunk(values, { expand: true }))
+    .then(response => {
+      dispatch(savePayoutDetailsSuccess());
+      return response;
+    })
     .catch(() => dispatch(savePayoutDetailsError()));
 };
 
 export const loadData = () => (dispatch, getState, sdk) => {
   // Clear state so that previously loaded data is not visible
   // in case this page load fails.
-  dispatch(setInitialState());
+  dispatch(setInitialValues());
 
-  return dispatch(fetchCurrentUser());
+  return dispatch(fetchCurrentUser())
+    .then(response => {
+      const currentUser = getState().user.currentUser;
+      if (currentUser && currentUser.stripeAccount) {
+        dispatch(fetchStripeAccount());
+      }
+      return response;
+    })
+    .catch(e => {
+      throw e;
+    });
 };

src/containers/StripePayoutPage/StripePayoutPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { bool, func } from 'prop-types';
+import { bool, func, oneOf, shape } from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import routeConfiguration from '../../routeConfiguration';
+import config from '../../config';
+import { createResourceLocatorString } from '../../util/routes';
 import { FormattedMessage, injectIntl, intlShape } from '../../util/reactIntl';
 import { ensureCurrentUser } from '../../util/data';
 import { propTypes } from '../../util/types';
 import { isScrollingDisabled } from '../../ducks/UI.duck';
-import { stripeAccountClearError } from '../../ducks/stripeConnectAccount.duck';
 import {
+  stripeAccountClearError,
+  getStripeConnectAccountLink,
+} from '../../ducks/stripeConnectAccount.duck';
+import {
+  NamedRedirect,
   LayoutSideNavigation,
   LayoutWrapperMain,
   LayoutWrapperAccountSettingsSideNav,
@@ -15,43 +22,115 @@ import {
   LayoutWrapperFooter,
   Footer,
   Page,
+  StripeConnectAccountStatusBox,
   UserNav,
 } from '../../components';
+import { StripeConnectAccountForm } from '../../forms';
 import { TopbarContainer } from '..';
 import { savePayoutDetails, loadData } from './StripePayoutPage.duck';
 
 import css from './StripePayoutPage.css';
 
+const STRIPE_ONBOARDING_RETURN_URL_SUCCESS = 'success';
+const STRIPE_ONBOARDING_RETURN_URL_FAILURE = 'failure';
+const STRIPE_ONBOARDING_RETURN_URL_TYPES = [
+  STRIPE_ONBOARDING_RETURN_URL_SUCCESS,
+  STRIPE_ONBOARDING_RETURN_URL_FAILURE,
+];
+
+// Create return URL for the Stripe onboarding form
+const createReturnURL = (returnURLType, rootURL, routes) => {
+  const path = createResourceLocatorString(
+    'StripePayoutOnboardingPage',
+    routes,
+    { returnURLType },
+    {}
+  );
+  const root = rootURL.replace(/\/$/, '');
+  return `${root}${path}`;
+};
+
+// Get attribute: stripeAccountData
+const getStripeAccountData = stripeAccount =>
+  stripeAccount.attributes.stripeAccountData ? stripeAccount.attributes.stripeAccountData : null;
+
+// Get last 4 digits of bank account returned in Stripe account
+const getBankAccountLast4Digits = stripeAccountData =>
+  stripeAccountData && stripeAccountData.external_accounts.data.length > 0
+    ? stripeAccountData.external_accounts.data[0].last4
+    : null;
+
+// Check if there's requirements on selected type: 'past_due', 'currently_due' etc.
+const hasRequirements = (stripeAccountData, requirementType) =>
+  stripeAccountData != null && stripeAccountData.requirements[requirementType].length > 0;
+
+// Redirect user to Stripe's hosted Connect account onboarding form
+const handleGetStripeConnectAccountLinkFn = (getLinkFn, commonParams) => type => () => {
+  getLinkFn({ type, ...commonParams }).then(url => {
+    window.location.href = url;
+  });
+};
+
 export const StripePayoutPageComponent = props => {
   const {
     currentUser,
     scrollingDisabled,
-    // createStripeAccountError,
-    // onPayoutDetailsFormChange,
-    // onPayoutDetailsFormSubmit,
-    // payoutDetailsSaveInProgress,
-    // payoutDetailsSaved,
+    getAccountLinkInProgress,
+    createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccountFetched,
+    stripeAccount,
+    onPayoutDetailsFormChange,
+    onPayoutDetailsFormSubmit,
+    onGetStripeConnectAccountLink,
+    payoutDetailsSaveInProgress,
+    payoutDetailsSaved,
+    params,
     intl,
   } = props;
 
+  const { returnURLType } = params;
   const ensuredCurrentUser = ensureCurrentUser(currentUser);
   const currentUserLoaded = !!ensuredCurrentUser.id;
-  const stripeConnected =
-    currentUserLoaded &&
-    !!ensuredCurrentUser.stripeAccount &&
-    !!ensuredCurrentUser.stripeAccount.id;
+  const stripeConnected = currentUserLoaded && !!stripeAccount && !!stripeAccount.id;
 
   const title = intl.formatMessage({ id: 'StripePayoutPage.title' });
 
-  let message = <FormattedMessage id="StripePayoutPage.loadingData" />;
+  const formDisabled = getAccountLinkInProgress;
 
-  if (currentUserLoaded && stripeConnected) {
-    message = <p>TODO: show edit verification information</p>;
-  } else if (currentUserLoaded && !stripeConnected) {
-    message = <FormattedMessage id="StripePayoutPage.stripeNotConnected" />;
+  const rootURL = config.canonicalRootURL;
+  const routes = routeConfiguration();
+  const successURL = createReturnURL(STRIPE_ONBOARDING_RETURN_URL_SUCCESS, rootURL, routes);
+  const failureURL = createReturnURL(STRIPE_ONBOARDING_RETURN_URL_FAILURE, rootURL, routes);
+
+  const accountId = stripeConnected ? stripeAccount.id : null;
+  const stripeAccountData = stripeConnected ? getStripeAccountData(stripeAccount) : null;
+  const requirementsMissing =
+    stripeAccount &&
+    (hasRequirements(stripeAccountData, 'past_due') ||
+      hasRequirements(stripeAccountData, 'currently_due'));
+
+  const savedCountry = stripeAccountData ? stripeAccountData.country : null;
+
+  const handleGetStripeConnectAccountLink = handleGetStripeConnectAccountLinkFn(
+    onGetStripeConnectAccountLink,
+    {
+      accountId,
+      successURL,
+      failureURL,
+    }
+  );
+
+  const returnedNormallyFromStripe = returnURLType === STRIPE_ONBOARDING_RETURN_URL_SUCCESS;
+  const showVerificationError = returnURLType === STRIPE_ONBOARDING_RETURN_URL_FAILURE;
+  const showVerificationNeeded = stripeConnected && requirementsMissing;
+
+  // Redirect from success URL to basic path for StripePayoutPage
+  if (returnedNormallyFromStripe && stripeConnected && !requirementsMissing) {
+    return <NamedRedirect name="StripePayoutPage" />;
   }
 
-  const form = <p>TODO: Connect Onboarding flow</p>;
   return (
     <Page title={title} scrollingDisabled={scrollingDisabled}>
       <LayoutSideNavigation>
@@ -69,8 +148,47 @@ export const StripePayoutPageComponent = props => {
             <h1 className={css.title}>
               <FormattedMessage id="StripePayoutPage.heading" />
             </h1>
-            {message}
-            {form}
+            {!currentUserLoaded ? (
+              <FormattedMessage id="StripePayoutPage.loadingData" />
+            ) : (
+              <StripeConnectAccountForm
+                disabled={formDisabled}
+                inProgress={payoutDetailsSaveInProgress}
+                ready={payoutDetailsSaved}
+                stripeBankAccountLastDigits={getBankAccountLast4Digits(stripeAccountData)}
+                savedCountry={savedCountry}
+                submitButtonText={intl.formatMessage({
+                  id: 'StripePayoutPage.submitButtonText',
+                })}
+                stripeAccountError={
+                  createStripeAccountError || updateStripeAccountError || fetchStripeAccountError
+                }
+                stripeAccountFetched={stripeAccountFetched}
+                onChange={onPayoutDetailsFormChange}
+                onSubmit={onPayoutDetailsFormSubmit}
+                onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink}
+                stripeConnected={stripeConnected}
+              >
+                {stripeConnected && (showVerificationError || showVerificationNeeded) ? (
+                  <StripeConnectAccountStatusBox
+                    type={showVerificationError ? 'verificationError' : 'verificationNeeded'}
+                    inProgress={getAccountLinkInProgress}
+                    onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink(
+                      'custom_account_verification'
+                    )}
+                  />
+                ) : stripeConnected && savedCountry ? (
+                  <StripeConnectAccountStatusBox
+                    type="verificationSuccess"
+                    inProgress={getAccountLinkInProgress}
+                    disabled={payoutDetailsSaveInProgress}
+                    onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink(
+                      'custom_account_update'
+                    )}
+                  />
+                ) : null}
+              </StripeConnectAccountForm>
+            )}
           </div>
         </LayoutWrapperMain>
         <LayoutWrapperFooter>
@@ -84,29 +202,55 @@ export const StripePayoutPageComponent = props => {
 StripePayoutPageComponent.defaultProps = {
   currentUser: null,
   createStripeAccountError: null,
+  updateStripeAccountError: null,
+  fetchStripeAccountError: null,
+  stripeAccount: null,
+  params: {
+    returnURLType: null,
+  },
 };
 
 StripePayoutPageComponent.propTypes = {
   currentUser: propTypes.currentUser,
   scrollingDisabled: bool.isRequired,
+  getAccountLinkInProgress: bool.isRequired,
   payoutDetailsSaveInProgress: bool.isRequired,
   createStripeAccountError: propTypes.error,
+  updateStripeAccountError: propTypes.error,
+  fetchStripeAccountError: propTypes.error,
+  stripeAccount: propTypes.stripeAccount,
   payoutDetailsSaved: bool.isRequired,
 
   onPayoutDetailsFormChange: func.isRequired,
   onPayoutDetailsFormSubmit: func.isRequired,
+  onGetStripeConnectAccountLink: func.isRequired,
+  params: shape({
+    returnURLType: oneOf(STRIPE_ONBOARDING_RETURN_URL_TYPES),
+  }),
 
   // from injectIntl
   intl: intlShape.isRequired,
 };
 
 const mapStateToProps = state => {
-  const { createStripeAccountError } = state.stripe;
+  const {
+    getAccountLinkInProgress,
+    createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccount,
+    stripeAccountFetched,
+  } = state.stripeConnectAccount;
   const { currentUser } = state.user;
   const { payoutDetailsSaveInProgress, payoutDetailsSaved } = state.StripePayoutPage;
   return {
     currentUser,
+    getAccountLinkInProgress,
     createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccount,
+    stripeAccountFetched,
     payoutDetailsSaveInProgress,
     payoutDetailsSaved,
     scrollingDisabled: isScrollingDisabled(state),
@@ -115,7 +259,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   onPayoutDetailsFormChange: () => dispatch(stripeAccountClearError()),
-  onPayoutDetailsFormSubmit: values => dispatch(savePayoutDetails(values)),
+  onPayoutDetailsFormSubmit: (isUpdateCall, values) =>
+    dispatch(savePayoutDetails(isUpdateCall, values)),
+  onGetStripeConnectAccountLink: params => dispatch(getStripeConnectAccountLink(params)),
 });
 
 const StripePayoutPage = compose(

robotsrng/flex-template-web

src/containers/StripePayoutPage/StripePayoutPage.duck.js
@@ -1,11 +1,14 @@
+import pick from 'lodash/pick';
 import {
   createStripeAccount,
+  updateStripeAccount,
+  fetchStripeAccount,
 } from '../../ducks/stripeConnectAccount.duck';
 import { fetchCurrentUser } from '../../ducks/user.duck';
 
 // ================ Action types ================ //
 
-export const SET_INITIAL_STATE = 'app/StripePayoutPage/SET_INITIAL_STATE';
+export const SET_INITIAL_VALUES = 'app/StripePayoutPage/SET_INITIAL_VALUES';
 export const SAVE_PAYOUT_DETAILS_REQUEST = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_REQUEST';
 export const SAVE_PAYOUT_DETAILS_SUCCESS = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_SUCCESS';
 export const SAVE_PAYOUT_DETAILS_ERROR = 'app/StripePayoutPage/SAVE_PAYOUT_DETAILS_ERROR';
@@ -15,13 +18,14 @@ export const SAVE_PAYOUT_DETAILS_ERROR = 'app/StripePayoutPage/SAVE_PAYOUT_DETAI
 const initialState = {
   payoutDetailsSaveInProgress: false,
   payoutDetailsSaved: false,
+  fromReturnURL: false,
 };
 
-export default function payoutPreferencesPageReducer(state = initialState, action = {}) {
-  const { type } = action;
+export default function reducer(state = initialState, action = {}) {
+  const { type, payload } = action;
   switch (type) {
-    case SET_INITIAL_STATE:
-      return initialState;
+    case SET_INITIAL_VALUES:
+      return { ...initialState, ...payload };
 
     case SAVE_PAYOUT_DETAILS_REQUEST:
       return { ...state, payoutDetailsSaveInProgress: true };
@@ -37,8 +41,9 @@ export default function payoutPreferencesPageReducer(state = initialState, actio
 
 // ================ Action creators ================ //
 
-export const setInitialState = () => ({
-  type: SET_INITIAL_STATE,
+export const setInitialValues = initialValues => ({
+  type: SET_INITIAL_VALUES,
+  payload: pick(initialValues, Object.keys(initialState)),
 });
 
 export const savePayoutDetailsRequest = () => ({
@@ -53,18 +58,28 @@ export const savePayoutDetailsSuccess = () => ({
 
 // ================ Thunks ================ //
 
-export const savePayoutDetails = values => (dispatch, getState, sdk) => {
+export const savePayoutDetails = (values, isUpdateCall) => (dispatch, getState, sdk) => {
+  const upsertThunk = isUpdateCall ? updateStripeAccount : createStripeAccount;
   dispatch(savePayoutDetailsRequest());
 
-  return dispatch(createStripeAccount(values))
-    .then(() => dispatch(savePayoutDetailsSuccess()))
+  return dispatch(upsertThunk(values, { expand: true }))
+    .then(response => {
+      dispatch(savePayoutDetailsSuccess());
+      return response;
+    })
     .catch(() => dispatch(savePayoutDetailsError()));
 };
 
 export const loadData = () => (dispatch, getState, sdk) => {
   // Clear state so that previously loaded data is not visible
   // in case this page load fails.
-  dispatch(setInitialState());
+  dispatch(setInitialValues());
 
-  return dispatch(fetchCurrentUser());
+  return dispatch(fetchCurrentUser()).then(response => {
+    const currentUser = getState().user.currentUser;
+    if (currentUser && currentUser.stripeAccount) {
+      dispatch(fetchStripeAccount());
+    }
+    return response;
+  });
 };

src/containers/StripePayoutPage/StripePayoutPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { bool, func } from 'prop-types';
+import { bool, func, oneOf, shape } from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import routeConfiguration from '../../routeConfiguration';
+import config from '../../config';
+import { createResourceLocatorString } from '../../util/routes';
 import { FormattedMessage, injectIntl, intlShape } from '../../util/reactIntl';
 import { ensureCurrentUser } from '../../util/data';
 import { propTypes } from '../../util/types';
 import { isScrollingDisabled } from '../../ducks/UI.duck';
-import { stripeAccountClearError } from '../../ducks/stripeConnectAccount.duck';
 import {
+  stripeAccountClearError,
+  getStripeConnectAccountLink,
+} from '../../ducks/stripeConnectAccount.duck';
+import {
+  NamedRedirect,
   LayoutSideNavigation,
   LayoutWrapperMain,
   LayoutWrapperAccountSettingsSideNav,
@@ -15,43 +22,119 @@ import {
   LayoutWrapperFooter,
   Footer,
   Page,
+  StripeConnectAccountStatusBox,
   UserNav,
 } from '../../components';
+import { StripeConnectAccountForm } from '../../forms';
 import { TopbarContainer } from '..';
 import { savePayoutDetails, loadData } from './StripePayoutPage.duck';
 
 import css from './StripePayoutPage.css';
 
+const STRIPE_ONBOARDING_RETURN_URL_SUCCESS = 'success';
+const STRIPE_ONBOARDING_RETURN_URL_FAILURE = 'failure';
+const STRIPE_ONBOARDING_RETURN_URL_TYPES = [
+  STRIPE_ONBOARDING_RETURN_URL_SUCCESS,
+  STRIPE_ONBOARDING_RETURN_URL_FAILURE,
+];
+
+// Create return URL for the Stripe onboarding form
+const createReturnURL = (returnURLType, rootURL, routes) => {
+  const path = createResourceLocatorString(
+    'StripePayoutOnboardingPage',
+    routes,
+    { returnURLType },
+    {}
+  );
+  const root = rootURL.replace(/\/$/, '');
+  return `${root}${path}`;
+};
+
+// Get attribute: stripeAccountData
+const getStripeAccountData = stripeAccount => stripeAccount.attributes.stripeAccountData || null;
+
+// Get last 4 digits of bank account returned in Stripe account
+const getBankAccountLast4Digits = stripeAccountData =>
+  stripeAccountData && stripeAccountData.external_accounts.data.length > 0
+    ? stripeAccountData.external_accounts.data[0].last4
+    : null;
+
+// Check if there's requirements on selected type: 'past_due', 'currently_due' etc.
+const hasRequirements = (stripeAccountData, requirementType) =>
+  stripeAccountData != null &&
+  stripeAccountData.requirements &&
+  Array.isArray(stripeAccountData.requirements[requirementType]) &&
+  stripeAccountData.requirements[requirementType].length > 0;
+
+// Redirect user to Stripe's hosted Connect account onboarding form
+const handleGetStripeConnectAccountLinkFn = (getLinkFn, commonParams) => type => () => {
+  getLinkFn({ type, ...commonParams })
+    .then(url => {
+      window.location.href = url;
+    })
+    .catch(err => console.error(err));
+};
+
 export const StripePayoutPageComponent = props => {
   const {
     currentUser,
     scrollingDisabled,
-    // createStripeAccountError,
-    // onPayoutDetailsFormChange,
-    // onPayoutDetailsFormSubmit,
-    // payoutDetailsSaveInProgress,
-    // payoutDetailsSaved,
+    getAccountLinkInProgress,
+    createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccountFetched,
+    stripeAccount,
+    onPayoutDetailsFormChange,
+    onPayoutDetailsFormSubmit,
+    onGetStripeConnectAccountLink,
+    payoutDetailsSaveInProgress,
+    payoutDetailsSaved,
+    params,
     intl,
   } = props;
 
+  const { returnURLType } = params;
   const ensuredCurrentUser = ensureCurrentUser(currentUser);
   const currentUserLoaded = !!ensuredCurrentUser.id;
-  const stripeConnected =
-    currentUserLoaded &&
-    !!ensuredCurrentUser.stripeAccount &&
-    !!ensuredCurrentUser.stripeAccount.id;
+  const stripeConnected = currentUserLoaded && !!stripeAccount && !!stripeAccount.id;
 
   const title = intl.formatMessage({ id: 'StripePayoutPage.title' });
 
-  let message = <FormattedMessage id="StripePayoutPage.loadingData" />;
+  const formDisabled = getAccountLinkInProgress;
 
-  if (currentUserLoaded && stripeConnected) {
-    message = <p>TODO: show edit verification information</p>;
-  } else if (currentUserLoaded && !stripeConnected) {
-    message = <FormattedMessage id="StripePayoutPage.stripeNotConnected" />;
+  const rootURL = config.canonicalRootURL;
+  const routes = routeConfiguration();
+  const successURL = createReturnURL(STRIPE_ONBOARDING_RETURN_URL_SUCCESS, rootURL, routes);
+  const failureURL = createReturnURL(STRIPE_ONBOARDING_RETURN_URL_FAILURE, rootURL, routes);
+
+  const accountId = stripeConnected ? stripeAccount.id : null;
+  const stripeAccountData = stripeConnected ? getStripeAccountData(stripeAccount) : null;
+  const requirementsMissing =
+    stripeAccount &&
+    (hasRequirements(stripeAccountData, 'past_due') ||
+      hasRequirements(stripeAccountData, 'currently_due'));
+
+  const savedCountry = stripeAccountData ? stripeAccountData.country : null;
+
+  const handleGetStripeConnectAccountLink = handleGetStripeConnectAccountLinkFn(
+    onGetStripeConnectAccountLink,
+    {
+      accountId,
+      successURL,
+      failureURL,
+    }
+  );
+
+  const returnedNormallyFromStripe = returnURLType === STRIPE_ONBOARDING_RETURN_URL_SUCCESS;
+  const showVerificationError = returnURLType === STRIPE_ONBOARDING_RETURN_URL_FAILURE;
+  const showVerificationNeeded = stripeConnected && requirementsMissing;
+
+  // Redirect from success URL to basic path for StripePayoutPage
+  if (returnedNormallyFromStripe && stripeConnected && !requirementsMissing) {
+    return <NamedRedirect name="StripePayoutPage" />;
   }
 
-  const form = <p>TODO: Connect Onboarding flow</p>;
   return (
     <Page title={title} scrollingDisabled={scrollingDisabled}>
       <LayoutSideNavigation>
@@ -69,8 +152,47 @@ export const StripePayoutPageComponent = props => {
             <h1 className={css.title}>
               <FormattedMessage id="StripePayoutPage.heading" />
             </h1>
-            {message}
-            {form}
+            {!currentUserLoaded ? (
+              <FormattedMessage id="StripePayoutPage.loadingData" />
+            ) : (
+              <StripeConnectAccountForm
+                disabled={formDisabled}
+                inProgress={payoutDetailsSaveInProgress}
+                ready={payoutDetailsSaved}
+                stripeBankAccountLastDigits={getBankAccountLast4Digits(stripeAccountData)}
+                savedCountry={savedCountry}
+                submitButtonText={intl.formatMessage({
+                  id: 'StripePayoutPage.submitButtonText',
+                })}
+                stripeAccountError={
+                  createStripeAccountError || updateStripeAccountError || fetchStripeAccountError
+                }
+                stripeAccountFetched={stripeAccountFetched}
+                onChange={onPayoutDetailsFormChange}
+                onSubmit={onPayoutDetailsFormSubmit}
+                onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink}
+                stripeConnected={stripeConnected}
+              >
+                {stripeConnected && (showVerificationError || showVerificationNeeded) ? (
+                  <StripeConnectAccountStatusBox
+                    type={showVerificationError ? 'verificationError' : 'verificationNeeded'}
+                    inProgress={getAccountLinkInProgress}
+                    onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink(
+                      'custom_account_verification'
+                    )}
+                  />
+                ) : stripeConnected && savedCountry ? (
+                  <StripeConnectAccountStatusBox
+                    type="verificationSuccess"
+                    inProgress={getAccountLinkInProgress}
+                    disabled={payoutDetailsSaveInProgress}
+                    onGetStripeConnectAccountLink={handleGetStripeConnectAccountLink(
+                      'custom_account_update'
+                    )}
+                  />
+                ) : null}
+              </StripeConnectAccountForm>
+            )}
           </div>
         </LayoutWrapperMain>
         <LayoutWrapperFooter>
@@ -84,29 +206,55 @@ export const StripePayoutPageComponent = props => {
 StripePayoutPageComponent.defaultProps = {
   currentUser: null,
   createStripeAccountError: null,
+  updateStripeAccountError: null,
+  fetchStripeAccountError: null,
+  stripeAccount: null,
+  params: {
+    returnURLType: null,
+  },
 };
 
 StripePayoutPageComponent.propTypes = {
   currentUser: propTypes.currentUser,
   scrollingDisabled: bool.isRequired,
+  getAccountLinkInProgress: bool.isRequired,
   payoutDetailsSaveInProgress: bool.isRequired,
   createStripeAccountError: propTypes.error,
+  updateStripeAccountError: propTypes.error,
+  fetchStripeAccountError: propTypes.error,
+  stripeAccount: propTypes.stripeAccount,
   payoutDetailsSaved: bool.isRequired,
 
   onPayoutDetailsFormChange: func.isRequired,
   onPayoutDetailsFormSubmit: func.isRequired,
+  onGetStripeConnectAccountLink: func.isRequired,
+  params: shape({
+    returnURLType: oneOf(STRIPE_ONBOARDING_RETURN_URL_TYPES),
+  }),
 
   // from injectIntl
   intl: intlShape.isRequired,
 };
 
 const mapStateToProps = state => {
-  const { createStripeAccountError } = state.stripe;
+  const {
+    getAccountLinkInProgress,
+    createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccount,
+    stripeAccountFetched,
+  } = state.stripeConnectAccount;
   const { currentUser } = state.user;
   const { payoutDetailsSaveInProgress, payoutDetailsSaved } = state.StripePayoutPage;
   return {
     currentUser,
+    getAccountLinkInProgress,
     createStripeAccountError,
+    updateStripeAccountError,
+    fetchStripeAccountError,
+    stripeAccount,
+    stripeAccountFetched,
     payoutDetailsSaveInProgress,
     payoutDetailsSaved,
     scrollingDisabled: isScrollingDisabled(state),
@@ -115,7 +263,9 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   onPayoutDetailsFormChange: () => dispatch(stripeAccountClearError()),
-  onPayoutDetailsFormSubmit: values => dispatch(savePayoutDetails(values)),
+  onPayoutDetailsFormSubmit: (values, isUpdateCall) =>
+    dispatch(savePayoutDetails(values, isUpdateCall)),
+  onGetStripeConnectAccountLink: params => dispatch(getStripeConnectAccountLink(params)),
 });
 
 const StripePayoutPage = compose(