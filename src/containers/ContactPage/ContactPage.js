3966ea84c4a8984115c6126027ecb8e066587cec
engelcalderon <engel199702@hotmail.com>
2020-02-12 14:24:00 -0600

    link to contact page footer, fixed image size on listing contact card

--- src/containers/ContactPage/ContactPage.js
+++ src/containers/ContactPage/ContactPage.js
@@ -7,6 +7,14 @@ import {
   LayoutWrapperFooter,
   Footer,
 } from '../../components';
+import {ListingContactCard} from './ListingContactCard';
+
+import css from './ContactPage.css';
+
+const AccountImage = require('./images/Account White _ Sidesuite copy 3.png');
+const CalendarImage = require('./images/Calendar White _ Bookings _ Sidesuite copy 3.png');
+const CreditCardImage = require('./images/Credit Card White _ Payment Method _ Sidesuite copy 3.png');
+
 const ContactPage = () => {
   // const handleResponse = data => {
   //   console.log(data);
@@ -16,6 +24,22 @@ const ContactPage = () => {
   //   console.log(error);
   // };
   // prettier-ignore
+
+  const heading = (
+    <div className={css.heading}>
+      <h1>Select one bellow</h1>
+      <p>We are here to help, and will get back to you withing 24 hours. Most likely a lot sooner than that.</p>
+    </div>
+  )
+
+  const cards = (
+    <div className={css.listingHolder}>
+      <ListingContactCard icon={CalendarImage}  title='Booking and Disputes'/>
+      <ListingContactCard icon={CreditCardImage}  title='Payments and Refounds'/>
+      <ListingContactCard icon={AccountImage}  title='My sidesuite Account'/>
+    </div>
+  )
+
   return (
     <StaticPage
       title="Contact"
@@ -32,7 +56,8 @@ const ContactPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain>
-
+          {heading}
+          {cards}
         </LayoutWrapperMain>
 
         <LayoutWrapperFooter>
---
+++ src/containers/ContactPage/ListingContactCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import css from './ListingContactCard.css';
+
+export const ListingContactCard = ({ icon, title }) => {
+    return (
+        <div className={css.root}>
+            <img className={css.iconImage} src={icon} />
+            <h2 className={css.title}>{title}</h2>
+        </div>
+    )
+}
