0bf207b38d3eb133406e0ba965a86ad1c915152b
Jaime_Liu <jaime.liu@proximitycr.com>
2020-03-09 11:00:46 -0600

    Spiner section calculator

--- src/components/SectionCalculator/SectionCalculator.js
+++ src/components/SectionCalculator/SectionCalculator.js
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { Form as FinalForm } from 'react-final-form';
 import axios from 'axios';
-import { ListingCalculatorResultCard } from '../../components';
 
+import { ListingCalculatorResultCard } from '../../components';
 import css from './SectionCalculator.css';
-import { SocialMediaButtons } from '../../components';
+import { Form, SocialMediaButtons, Button } from '../../components';
 
 const SectionCalculator = props => {
-  const { rootClassName, className, title, description } = props;
+  const { title, description } = props;
 
-  const classes = classNames(rootClassName || css.root, className);
   const [showCalculatorResult, setShowCalculatorResult] = useState(true);
   const [socialMediaSelected, setSocialMediaSelected] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [calculatedData, setCalculatedData] = useState();
+  const [inProgress, setInProgress] = useState(false);
+
   const disabled = !inputValue || !socialMediaSelected;
   const handleOnClick = e => {
+    e.preventDefault();
     setSocialMediaSelected(e.target.value);
   };
   const handleCalculate = _ => {
+    setInProgress(true);
     const data = { service: socialMediaSelected, username: inputValue.trim() };
     axios
       .post('/api/calculate', data)
@@ -27,8 +31,12 @@ const SectionCalculator = props => {
         console.log(res);
         setCalculatedData(res);
         setShowCalculatorResult(true);
+        setInProgress(false);
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+        setInProgress(false);
+      });
   };
   const handleOnChange = value => {
     if (showCalculatorResult) {
@@ -56,27 +64,56 @@ const SectionCalculator = props => {
       )
     ) : null;
   return (
-    <div className={classes}>
-      <div className={css.title}>
-        <h1>{title}</h1>
-      </div>
-      <div className={css.description}>
-        <p>{description}</p>
-      </div>
-      <SocialMediaButtons handleOnClick={handleOnClick} socialMediaSelected={socialMediaSelected} />
-      <div className={css.calculatorInput}>
-        <p className={css.inputTitle}>Username</p>
-        <input
-          placeholder="username"
-          onChange={e => handleOnChange(e.target.value)}
-          value={inputValue}
-        ></input>
-        <button className={css.button} onClick={handleCalculate} disabled={disabled}>
-          Calculate
-        </button>
-        <div className={css.calculator}>{calculatorResult}</div>
-      </div>
-    </div>
+    <FinalForm
+      {...props}
+      onSubmit={handleCalculate}
+      render={fieldRenderProps => {
+        const {
+          className,
+          rootClassName,
+          handleSubmit,
+          ready,
+          updated,
+          pristine,
+        } = fieldRenderProps;
+
+        const classes = classNames(rootClassName || css.root, className);
+        const submitReady = (updated && pristine) || ready;
+        return (
+          <Form className={classes} onSubmit={handleSubmit}>
+            <div className={css.title}>
+              <h1>{title}</h1>
+            </div>
+            <div className={css.description}>
+              <p>{description}</p>
+            </div>
+            <SocialMediaButtons
+              handleOnClick={handleOnClick}
+              socialMediaSelected={socialMediaSelected}
+            />
+            <div className={css.calculatorInput}>
+              <p className={css.inputTitle}>Username</p>
+              <input
+                placeholder="username"
+                onChange={e => handleOnChange(e.target.value)}
+                value={inputValue}
+              ></input>
+              <Button
+                className={css.button}
+                type="submit"
+                onClick={handleCalculate}
+                disabled={disabled}
+                inProgress={inProgress}
+                ready={submitReady}
+              >
+                Calculate
+              </Button>
+              <div className={css.calculator}>{calculatorResult}</div>
+            </div>
+          </Form>
+        );
+      }}
+    />
   );
 };
 
